842c0b35a23dca1fdf45272a1851450b69803efa
Astrid Hanghøj <astridhanghoej@gmail.com>
2021-08-02 11:38:23 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    myVar = 0
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    myVar += 1
+})
+function myFunction () {
+    // write this function only with programming language of your choosing
+    return "Delete this and write the program"
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    myFunction()
+})
+let myVar = 0
+myVar = 0
