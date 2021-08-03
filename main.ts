04f063cfaccfb4d9fe49b0b79f7c05395c98ca07
Astrid Hanghøj <astridhanghoej@gmail.com>
2021-08-03 11:28:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     myVar += 1
 })
 function myFunction () {
-    game.splash("My var is " + ("" + myVar))
+    game.splash("" + "My var is " + myVar)
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     myFunction()
 })
 let myVar = 0
-myVar = 2
+myVar = 0
